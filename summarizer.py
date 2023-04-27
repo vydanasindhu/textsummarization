fb8e28e3e937bbbee4e02175a337c080ed7385c6
kavvya97 <kavsramarathnam97@gmail.com>
2023-04-26 22:59:50 -0500

    Added ensemble summarization method

--- summarizer.py
+++ summarizer.py
@@ -6,9 +6,29 @@ from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
 from heapq import nlargest
 from collections import Counter
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.summarizers.lsa import LsaSummarizer
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.utils import get_stop_words
+import math
 
-def summarize(text, number_of_sentences):
-    nlp = spacy.load('en_core_web_sm')
+app = Flask(__name__)
+CORS(app)
+
+# Post Processing techniques
+def post_process_summary(summary):
+    if not summary:
+        return summary
+    summary = summary\
+        .replace('\xa0', ' ')\
+            .strip('.,!?' )[0]\
+                .upper() + summary[1:] + '.' if summary[-1] not in '.!?' \
+                    else summary[0].upper() + summary[1:]   
+    return summary
+
+# Summarization algorithm using spacy
+def summarize_using_spacy(text, number_of_sentences):
+    nlp = spacy.load('en_core_web_md')
     doc= nlp(text)
 
     # Token Filtering
@@ -38,7 +58,6 @@ def summarize(text, number_of_sentences):
                 else:
                     sent_strength[sent]=freq_word[word.text]
 
-    # String summarization
     summarized_sentences = nlargest(number_of_sentences, sent_strength, key=sent_strength.get)
 
     final_sentences = [ w.text for w in summarized_sentences ]
@@ -46,17 +65,37 @@ def summarize(text, number_of_sentences):
 
     # Get top-5 tags
     tags = [tag for tag, _ in freq_word.most_common(5)]
-    return summary, tags
+    return post_process_summary(summary), tags
 
-app = Flask(__name__)
-CORS(app)
+#Summarization using Sumy
+def summarize_using_sumy(text, number_of_sentences):
+    LANGUAGE = "english"
+    parser = PlaintextParser.from_string(text, Tokenizer(LANGUAGE))
+    summarizer = LsaSummarizer()
+    summarizer.stop_words = get_stop_words(LANGUAGE)
+    summarized_sentences = summarizer(parser.document, number_of_sentences)
+    summary = ' '.join([str(sentence) for sentence in summarized_sentences])
+    return post_process_summary(summary)
+
+def ensemble_summarization(text, count, sumy_weight=0.8, spacy_weight=0.2):
+    spacy_sentences =int(math.ceil(count * spacy_weight))
+    sumy_sentences = count - spacy_sentences
+    # Spacy summarizer
+    spacy_summary, top_5_tags = summarize_using_spacy(text, spacy_sentences)
+    # Sumy summarizer
+    sumy_summary = summarize_using_sumy(text, sumy_sentences)
+
+    combined_summary = sumy_summary + ' ' + spacy_summary
+    return post_process_summary(combined_summary), top_5_tags
+
+
 @app.route('/summarize', methods=['POST'])
 def example_api():
     data = request.json
     message = data['message']
     numSentences = data.get('numSentences', 2)  # Get the number of sentences from the request data
     numSentences = max(1, int(numSentences))  # Ensure numSentences is at least 1
-    summary, tags = summarize(message, numSentences)  # Use the numSentences value when calling the summarize function
+    summary, tags = ensemble_summarization(message, numSentences)  # Use the numSentences value when calling the summarize function
     response_data = {'summary': summary, 'tags': tags}
     return jsonify(response_data)
 
