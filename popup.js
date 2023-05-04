5f982c2c3b79cfc928e5e5020b32dff8378db771
srikodav <sk121@illinois.edu>
2023-05-04 17:48:52 -0500

    All the changes

--- popup.js
+++ popup.js
@@ -3,51 +3,90 @@ document.addEventListener("DOMContentLoaded", () => {
   const summaryContainer = document.getElementById("summary");
   const tagContainer = document.getElementById("tags");
   const numSentencesInput = document.getElementById("num-sentences");
+  const sentimentContainer = document.getElementById("sentiment-container");
+  const copyCitationButton = document.getElementById("copy-citation");
+  const copySummaryButton = document.getElementById("copy-summary");
+  const resultsContainer = document.getElementById("results");
 
   summarizeButton.addEventListener("click", async () => {
-    const activeTab = await getActiveTab();
     const numSentences = numSentencesInput.value;
-    summaryContainer.innerText = ""
+    if (numSentences > 10) {
+      alert("The maximum limit is only 10 sentences to summarize.");
+      return;
+    }
+    const activeTab = await getActiveTab();
+    summaryContainer.innerText = "";
     const loadingIcon = document.getElementById("loading-icon");
     loadingIcon.style.display = "inline-block";
     const result = await chrome.scripting.executeScript({
       target: { tabId: activeTab.id },
       func: summarize,
-      args: [numSentences], 
+      args: [numSentences],
     });
     loadingIcon.style.display = "none";
+    resultsContainer.classList.remove("hidden");
     if (result[0].result) {
       summaryContainer.innerText = result[0].result["summary"] || "Summary not available";
       tagContainer.innerHTML = "";
       const tags = result[0].result["tags"] || [];
       if (tags.length == 0) {
-        tagContainer.innerHTML = "Tags not available"
+        tagContainer.innerHTML = "Tags not available";
       }
       tags.forEach(tag => {
-        const chip = document.createElement('span');
-        chip.className = 'chip';
+        const chip = document.createElement("span");
+        chip.className = "chip";
         chip.innerText = tag;
         tagContainer.appendChild(chip);
       });
-      // DISPLAY SENTIMENT & SUBJECTIVITY
-      // TODO: result[0].result["sentiment"] = 1 -> positive  = 0 -> neutral  = -1 -> negative 
-      // TODO: result[0].result["subjectivity"] = 1 -> objective  = 0 -> neutral  = -1 -> subjective
+      const sentiment = result[0].result["sentiment"];
+    switch (sentiment) {
+    case 1:
+    sentimentContainer.innerText = "\uD83D\uDE03"; // 😃
+    break;
+  case 0:
+    sentimentContainer.innerText = "\uD83D\uDE10"; // 😐
+    break;
+  case -1:
+    sentimentContainer.innerText = "\u2639\uFE0F"; // ☹️
+    break;
+  default:
+    sentimentContainer.innerText = "Sentiment not available";
+    break;
+}
+      copyCitationButton.addEventListener("click", () => {
+        const citation = `Citation: ${activeTab.title}. Retrieved from ${activeTab.url}`;
+        navigator.clipboard.writeText(citation).then(() => {
+          alert("Citation copied to clipboard!");
+        }, () => {
+          alert("Failed to copy citation.");
+        });
+      });
+
+      copySummaryButton.addEventListener("click", () => {
+        const summaryText = summaryContainer.innerText;
+        navigator.clipboard.writeText(summaryText).then(() => {
+          alert("Summary copied to clipboard!");
+        }, () => {
+          alert("Failed to copy summary.");
+        });
+      });
+
     } else {
       summaryContainer.innerText = "Summary not available";
       tagContainer.innerText = "Tags not available";
+      sentimentContainer.innerText = "Sentiment not available";
     }
   });
 });
-
 
 async function getActiveTab() {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   return tab;
 }
 
-async function summarize(numSentences) { 
+async function summarize(numSentences) {
   const message = window.location.href;
-  const data = { message: message, numSentences: numSentences }; 
+  const data = { message: message, numSentences: numSentences };
   try {
     const response = await fetch("http://localhost:8000/summarize", {
       method: "POST",
@@ -57,6 +96,7 @@ async function summarize(numSentences) {
       },
       body: JSON.stringify(data),
     });
+
     const result = await response.json();
     return result;
   } catch (error) {
