ff45cbebc526ee0272c1d737a5b68d94365d7395
srikodav <sk121@illinois.edu>
2023-05-05 16:24:26 -0500

    Final css Changes(Sentiment, Copy buttons)

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-let isSummary=false;
+let isSummary = false;
 let summaryText = "";
 document.addEventListener("DOMContentLoaded", () => {
   const summarizeButton = document.getElementById("summarize");
@@ -12,7 +12,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const numSentences = document.getElementById('num-sentences');
   const targetLang = document.getElementById("language-selection");
 
-  numSentences.addEventListener('input', function() {
+  copyCitationButton.classList.add("hidden");
+  copySummaryButton.classList.add("hidden");
+
+  numSentences.addEventListener('input', function () {
     if (parseInt(this.value) > 10) {
       this.value = 10;
     }
@@ -32,9 +35,14 @@ document.addEventListener("DOMContentLoaded", () => {
     loadingIcon.style.display = "none";
     resultsContainer.classList.remove("hidden");
     if (result[0].result) {
-      summaryText=result[0].result["summary"] || "Summary not available";
+      summaryText = result[0].result["summary"] || "Summary not available";
       summaryContainer.innerText = summaryText;
       isSummary = true;
+
+      // Show the "Copy Citation" and "Copy Summary" buttons
+      copyCitationButton.classList.remove("hidden");
+      copySummaryButton.classList.remove("hidden");
+
       tagContainer.innerHTML = "";
       const tags = result[0].result["tags"] || [];
       if (tags.length == 0) {
@@ -46,23 +54,25 @@ document.addEventListener("DOMContentLoaded", () => {
         chip.innerText = tag;
         tagContainer.appendChild(chip);
       });
-      const sentiment = result[0].result["sentiment"];
+      const sentiment =result[0].result["sentiment"];
       sentimentContainer.className = ""; 
       switch (sentiment) {
         case 1:
-          sentimentContainer.innerText = "\uD83D\uDE03"; // 😃
-          sentimentContainer.classList.add("positive");
+          sentimentContainer.innerText = "\u263A"; 
+          sentimentContainer.style.color = "green";
           break;
         case 0:
-          sentimentContainer.innerText = "\uD83D\uDE10"; // 😐
-          sentimentContainer.classList.add("neutral");
+          sentimentContainer.innerText = "\uD83D\uDE10";; 
           break;
+    
         case -1:
-          sentimentContainer.innerText = "\u2639\uFE0F"; // ☹️
-          sentimentContainer.classList.add("negative");
+          sentimentContainer.innerText = "\u2639"; 
+          sentimentContainer.style.color = "Red";
           break;
+
         default:
           sentimentContainer.innerText = "Sentiment not available";
+          sentimentContainer.style.color = ""; 
           break;
       }
       copyCitationButton.addEventListener("click", () => {
@@ -77,52 +87,55 @@ document.addEventListener("DOMContentLoaded", () => {
           alert("Failed to copy citation!");
         });
       });    
-
-    } else {
-      summaryContainer.innerText = "Summary not available";
-      tagContainer.innerText = "Tags not available";
-      sentimentContainer.innerText = "Sentiment not available";
     }
-  });
-
-  copySummaryButton.addEventListener("click", () => {
-    if (isSummary) {
-      navigator.clipboard.writeText(summaryText).then(() => {
-        const originalButton = copySummaryButton.innerText;
-        copySummaryButton.innerText = "Copied!";
-        setTimeout(() => {
-          copySummaryButton.innerText = originalButton;
-        }, 2000);
-      }, () => {
-        alert("Failed to copy summary.");
-      });
-    } else {
-      alert("No summary available to copy.");
-    }
-  });
-});
-
-async function getActiveTab() {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  return tab;
-}
-
-async function summarize(numSentences, targetLang) {
-  const message = window.location.href;
-  const data = { message: message, numSentences: numSentences, targetLang: targetLang };
-  try {
-    const response = await fetch("http://localhost:8000/summarize", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        "Accept-Charset": "utf-8"
-      },
-      body: JSON.stringify(data),
+      else {
+        summaryContainer.innerText = "Summary not available";
+        tagContainer.innerText = "Tags not available";
+        sentimentContainer.innerText = "Sentiment not available";
+        copyCitationButton.classList.add("hidden");
+        copySummaryButton.classList.add("hidden");
+      }
     });
-
-    const result = await response.json();
-    return result;
-  } catch (error) {
-    console.error(error);
+  
+    copySummaryButton.addEventListener("click", () => {
+      if (isSummary) {
+        navigator.clipboard.writeText(summaryText).then(() => {
+          const originalButton = copySummaryButton.innerText;
+          copySummaryButton.innerText = "Copied!";
+          setTimeout(() => {
+            copySummaryButton.innerText = originalButton;
+          }, 2000);
+        }, () => {
+          alert("Failed to copy summary.");
+        });
+      } else {
+        alert("No summary available to copy.");
+      }
+    });
+  });
+  
+  async function getActiveTab() {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    return tab;
   }
-}
+  
+  async function summarize(numSentences, targetLang) {
+    const message = window.location.href;
+    const data = { message: message, numSentences: numSentences, targetLang: targetLang };
+    try {
+      const response = await fetch("http://localhost:8000/summarize", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          "Accept-Charset": "utf-8"
+        },
+        body: JSON.stringify(data),
+      });
+  
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
